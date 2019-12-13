004700029ccb30f592a556e3925e6e3b513645c5
Wargast <lucbaudinaud@orange.fr>
2019-12-13 20:48:33 +0100

    Compile Eigen .sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,4 @@
 
 #!/usr/bin/env bash
 
-
-g++ -I $(PWD) -o launcher  src/main2.cpp src/source/Source.cpp  src/source/aperiodique.cpp src/source/periodique.cpp src/source/creneau.cpp src/source/echelon.cpp src/source/rectang.cpp src/source/sinus.cpp src/source/triang.cpp src/EDP/EDP.cpp src/EDP/EDP1.cpp src/EDP/exemple1.cpp src/EDP/circuit.cpp src/EDP/RC.cpp src/EDP/RCD.cpp src/EDP/EDP_2.cpp src/EDP/exemple2.cpp src/EDP/circuit2.cpp src/EDP/RLC.cpp src/EDP/RLCparallel.cpp
+g++ -I ./ -o launcher  src/main2.cpp src/source/Source.cpp  src/source/aperiodique.cpp src/source/periodique.cpp src/source/creneau.cpp src/source/echelon.cpp src/source/rectang.cpp src/source/sinus.cpp src/source/triang.cpp src/EDP/EDP.cpp src/EDP/EDP1.cpp src/EDP/exemple1.cpp src/EDP/circuit.cpp src/EDP/RC.cpp src/EDP/RCD.cpp src/EDP/EDP_2.cpp src/EDP/exemple2.cpp src/EDP/circuit2.cpp src/EDP/RLC.cpp src/EDP/RLCparallel.cpp
